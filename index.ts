97dd3c21fd391fba37d35029fc57f06a0f62174c
Hong0117 <51926425+shongc@users.noreply.github.com>
2023-02-21 11:59:08 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import Koa from "koa";
+import Router,{RouterContext} from "koa-router";
+import logger from "koa-logger";
+import json from "koa-json";
+
+const app: Koa = new Koa();
+const router: Router = new Router();
+
+router.get('/api/v1', async(ctx:RouterContext, next:any)=>{
+  ctx.body = { message: "Welcome to the blog API!"};
+  await next();
+});
+
+app.use(logger());
+app.use(json());
+app.use(router.routes());
+
+app.listen(10888);
+
+
+
