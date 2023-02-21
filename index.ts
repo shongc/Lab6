f8ad402a485c1f6542e7426c443ce5f2c86e6ac0
Hong0117 <51926425+shongc@users.noreply.github.com>
2023-02-21 13:11:53 +0000

    Complete five basic funcitons

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import Koa from "koa";
-import Router,{RouterContext} from "koa-router";
+// import Router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
-import {router as articles} from "./routes/articles";
+import { router as articles } from "./routes/articles";
 
 const app: Koa = new Koa();
-const router: Router = new Router();
+// const router: Router = new Router();
 
 
-const welcomeAPI=async(ctx:RouterContext, next:any)=>{
-  ctx.body = { message: "Welcome to the blog API!"};
-  await next();
-}
-router.get('/api/v1', welcomeAPI);
+// const welcomeAPI=async(ctx:RouterContext, next:any)=>{
+//   ctx.body = { message: "Welcome to the blog API!"};
+//   await next();
+// }
+// router.get('/api/v1', welcomeAPI);
 
 
 app.use(logger());
 app.use(json());
-app.use(router.routes());
+// app.use(router.routes());
 app.use(articles.routes());
 
 app.listen(10888);
--- routes/articles.ts
+++ routes/articles.ts
@@ -1,28 +1,59 @@
-import Router, {RouterContext} from "koa-router";
+import Router, { RouterContext } from "koa-router";
 import bodyParser from "koa-bodyparser";
 
 const articles = [
-  {title: 'hello article', fullText:'some text here to fill the body'},
-  {title: 'another article', fullText:'again here is some text here to fill the body'},
-  {title: 'conventry university', fullText:'some news about conventry university'},
-  {title: 'smart campus', fullText:'smart campus hahaha ?.?'}
+  { title: 'hello article', fullText: 'some text here to fill the body' },
+  { title: 'another article', fullText: 'again here is some text here to fill the body' },
+  { title: 'conventry university', fullText: 'some news about conventry university' },
+  { title: 'smart campus', fullText: 'smart campus hahaha ?.?' }
 ]
 
-const router = new Router({prefix:'/api/v1/articles'});
+const router = new Router({ prefix: '/api/v1/articles' });
 
-const getAll = async(ctx:RouterContext, next:any)=>{
+const getAll = async (ctx: RouterContext, next: any) => {
   ctx.body = articles;
   await next();
 };
-const getById = async(ctx:RouterContext, next:any)=>{};
-const createArticle = async(ctx:RouterContext, next:any)=>{};
-const updateArticle = async(ctx:RouterContext, next:any)=>{};
-const deleteArticle = async(ctx:RouterContext, next:any)=>{};
+const createArticle = async (ctx: RouterContext, next: any) => {
+  let { title, fullText } = ctx.request.body;
+  let newArticle = { title: title, fullText: fullText };
 
-router.get('/',getAll);
-router.post('/',bodyParser(),createArticle);
-router.get('/:id([0-9]{1,})',getById);
-router.put('/:id([0-9]{1,})',updateArticle);
-router.del('/:id([0-9]{1,})',deleteArticle);
+  articles.push(newArticle);
+  ctx.status = 201;
+  ctx.body = newArticle;
+  await next();
+}
+const getById = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id
+  if ((id < articles.length+1) && (id > 0)) {
+    ctx.body = articles[id - 1];
+  } else {
+    ctx.status = 404;
+  }
+  await next();
+};
 
-export {router};
+const updateArticle = async (ctx: RouterContext, next: any) => {
+
+  let { title, fullText } = ctx.request.body;
+  let id = +ctx.params.id
+  let currentArticle = articles[id];
+  let newArticle = { title: title, fullText: fullText };
+  articles[id] = newArticle;
+  
+  ctx.body = newArticle;
+  
+  await next();
+};
+const deleteArticle = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id
+  articles.splice(articles.indexOf(id,0),1);
+};
+
+router.get('/', getAll);
+router.post('/', bodyParser(), createArticle);
+router.get('/:id([0-9]{1,})', getById);
+router.put('/:id([0-9]{1,})',bodyParser(), updateArticle);
+router.del('/:id([0-9]{1,})', deleteArticle);
+
+export { router };
